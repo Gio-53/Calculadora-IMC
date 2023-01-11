df7de631763db221809c7f7aa6d7c8780e374e35
Giovani <108760391+Gio-53@users.noreply.github.com>
2023-01-11 12:59:07 -0300

    Add files via upload

---
+++ imc.js
@@ -0,0 +1,31 @@
+
+const calcular = document.getElementById('calcular');
+
+
+function imc(){
+    const nome = document.getElementById('nome').value;
+    const altura = document.getElementById('altura').value;
+    const peso = document.getElementById('peso').value;
+    const resultado = document.getElementById('resultado');
+
+    if(nome !== '' && altura!== '' && peso!==''){
+        const valorIMC =(peso/(altura*altura)).toFixed(2);
+        let classificacao = '';
+       if(valorIMC <18.5 ){
+            classificacao = 'abaixo do peso!';
+        } else if( valorIMC<25){
+            classificacao ='no peso ideal!';
+        }else if(valorIMC<30){
+            classificacao ='com sobrepeso!';
+        }else if(valorIMC<40){
+            classificacao ='com obesidade!';
+        }else{
+            classificacao ='com obesidade grave!';
+        }
+        resultado.textContent = `${nome} seu IMC é: ${valorIMC} e você está ${classificacao}`;
+    }else{
+       resultado.textContent = 'Preencha todos os campos!' ;
+    }
+
+}
+calcular.addEventListener('click',imc);
